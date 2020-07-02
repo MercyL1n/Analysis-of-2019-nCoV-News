78abb62f9991ee344e8d37792aeb6e18cd209813
MercyL1n <18506000485@163.com>
2020-07-02 23:47:02 +0800

    修了topmid的bug

--- src/components/common/TopMid.vue
+++ src/components/common/TopMid.vue
@@ -1,26 +1,11 @@
 <template>
   <div class="top-mid">
-    <!-- <div class="item" v-for="item in dataset" :key="item.id">
-      <span class="name">{{item.name}}
-        <strong>{{item.value}}</strong>
-      </span>
-      <span class="huanbi">环比
-        <strong :data-state="item.state">{{item.huanbi}}
-          <em>%</em>
-        </strong>
-      </span>
-    </div> -->
     <div class="midTitle">{{title}}</div>
   </div>
 </template>
 <script>
-  // import axios from 'axios'
-  // import api from '../../../src/assets/scripts/tool/api'
-  // import Data from '../../data/fantasy/castle/top'
-  // Data()
   export default {
     name: 'topMid',
-    props: ['title'],
     data () {
       return {
         title: '疫情新闻采集分析系统'
